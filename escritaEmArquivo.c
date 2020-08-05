9f951183a9e6b2350f73b00634a53d1ddca3e71f
Amarildo <69056178+AmarildoOliveira84@users.noreply.github.com>
2020-08-05 15:32:44 -0400

    Add files via upload

---
+++ escritaEmArquivo.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main() {
+    FILE *arq;
+    char pecas[100];
+    arq=fopen("pecas.txt", "w");
+
+    printf("Informe as pecas ou clique em 1 para sair: ");
+    fgets(pecas, 100, stdin);
+
+    if (arq) {
+
+        while (pecas[0]!='1') {
+            fputs(pecas, arq);
+
+            printf("Informe as pecas ou clique em 1 para sair: ");
+            fgets(pecas, 100, stdin);
+        }
+
+
+    }
+    else {
+
+        printf("Arquivo nao pode ser criado: ");
+    }
+
+    return 0;
+
+}
